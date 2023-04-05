ac7c136131e82835e2e257ac65fbadccb333737e
Thinh Pham-Van <phamvanthinh2001x@gmail.com>
2023-04-05 23:34:46 +0700

    🏠 Home and change bg header

---
+++ assets/js/main.js
@@ -0,0 +1,8 @@
+/* CHANGE BACKGROUND HEADER */
+const scrollHeader = () => {
+  const header = document.getElementById("header");
+  this.scrollY >= 50
+    ? header.classList.add("scroll-header")
+    : header.classList.remove("scroll-header");
+};
+window.addEventListener("scroll", scrollHeader);
